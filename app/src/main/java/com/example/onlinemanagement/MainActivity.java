4602f53551f78dd32ee281b0bc423378930c05c7
zahra <zahra3161380peyghan@gmail.com>
2021-06-03 18:36:38 +0430

    Check out the splash screen design.

--- app/src/main/java/com/example/onlinemanagement/MainActivity.java
+++ app/src/main/java/com/example/onlinemanagement/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.onlinemanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    LottieAnimationView money;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        money=findViewById(R.id.money);
+        money.animate().translationY(1600).setDuration(1000).setStartDelay(6000);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        },7000);
     }
 }
