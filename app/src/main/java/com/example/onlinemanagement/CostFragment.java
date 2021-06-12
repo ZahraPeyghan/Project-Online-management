a066ccc0995402595b7c9e4f9783ba026a98d43d
zahra <zahra3161380peyghan@gmail.com>
2021-06-13 00:53:20 +0430

    Cost fragment design finished.

---
+++ app/src/main/java/com/example/onlinemanagement/CostFragment.java
@@ -0,0 +1,25 @@
+package com.example.onlinemanagement;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class CostFragment extends Fragment {
+    public CostFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cost, container, false);
+    }
+}
