4f458d64eac12fea3213efd515af03bb97a6d379
zahra <zahra3161380peyghan@gmail.com>
2021-06-14 20:21:00 +0430

    Finishing adding and saving it in the database is complete.

--- app/src/main/java/com/example/onlinemanagement/DashboardActivity.java
+++ app/src/main/java/com/example/onlinemanagement/DashboardActivity.java
@@ -1,14 +1,23 @@
 package com.example.onlinemanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class DashboardActivity extends AppCompatActivity {
+import com.example.onlinemanagement.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
+public class DashboardActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.countent,new ReportFragment());
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/onlinemanagement/Income.java
@@ -0,0 +1,56 @@
+package com.example.onlinemanagement;
+
+public class Income {
+    private String price,grouping,account,date,discription;
+
+    public Income() {
+    }
+
+    public Income(String price, String grouping, String account, String date, String discription) {
+        this.price = price;
+        this.grouping = grouping;
+        this.account = account;
+        this.date = date;
+        this.discription = discription;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getGrouping() {
+        return grouping;
+    }
+
+    public void setGrouping(String grouping) {
+        this.grouping = grouping;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+}
